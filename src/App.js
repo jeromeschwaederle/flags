a90356d4b603015e63925e7f354fd93b298c1050
Jerome Schwaederle <schwaederle.jerome@gmail.com>
2022-05-30 19:06:30 +0200

    Start of layout finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
-}
+import classes from "./App.module.css";
+
+import Container from "./UI/Container";
+
+const App = () => {
+  return (
+    <Container>
+      <h1 className={classes.h1}>Titre exexptionel</h1>
+      <section className={classes.section}>
+        <h2>premier titre</h2>
+        <p>Un paragraphe très intéressant.</p>
+      </section>
+    </Container>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import "./index.css";
+import classes from "./index.module.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App className={classes.app} />
   </React.StrictMode>
 );
